e823f48ee5a6590da02c87a8b04ac7901c06cb43
Victor Garcia <victor.g64r@gmail.com>
2020-09-13 16:04:09 -0500

    Remove log from video component

--- src/components/Video/Video.component.jsx
+++ src/components/Video/Video.component.jsx
@@ -4,7 +4,6 @@ import useStyles from './styles';
 
 const Video = ({ item }) => {
   const classes = useStyles();
-  console.log(item);
   return (
     <CardActionArea className={classes.videoClass}>
       <img
