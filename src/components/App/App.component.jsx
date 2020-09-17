439707314e056ac4371f70dd362da9ba701330d2
Victor Garcia <victor.g64r@gmail.com>
2020-09-17 16:48:37 -0500

    Remove login path

--- src/components/App/App.component.jsx
+++ src/components/App/App.component.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import AuthProvider from '../../providers/Auth';
 import HomePage from '../../pages/Home';
-import LoginPage from '../../pages/Login';
 import NotFound from '../../pages/NotFound';
 import SecretPage from '../../pages/Secret';
 import Private from '../Private';
@@ -67,9 +66,6 @@ function App() {
             <Switch>
               <Route exact path="/">
                 <HomePage />
-              </Route>
-              <Route exact path="/login">
-                <LoginPage />
               </Route>
               <Route exact path="/video">
                 <VideoPlayer />
