787c34dc17d500577ee478538fe5bd7e7d008087
Victor Garcia <victor.g64r@gmail.com>
2020-09-17 16:48:03 -0500

    Remove useless code in Home

--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -1,44 +1,16 @@
 import React, { useRef, useContext } from 'react';
-import { Link, useHistory } from 'react-router-dom';
-
 import VideoList from '../../components/VideoList';
 import VideoContext from '../../state/VideoContext';
-import { useAuth } from '../../providers/Auth';
-
 import './Home.styles.css';
 
 function HomePage() {
-  const history = useHistory();
   const sectionRef = useRef(null);
-  const { authenticated, logout } = useAuth();
 
   const { videos } = useContext(VideoContext);
-
-  function deAuthenticate(event) {
-    event.preventDefault();
-    logout();
-    history.push('/');
-  }
 
   return (
     <section className="homepage" ref={sectionRef}>
       <VideoList videos={videos} parent="homepage" />
-      {authenticated ? (
-        <>
-          <h2>Good to have you back</h2>
-          <span>
-            <Link to="/" onClick={deAuthenticate}>
-              ← logout
-            </Link>
-            <span className="separator" />
-            <Link to="/secret">show me something cool →</Link>
-          </span>
-        </>
-      ) : (
-        <>
-          <Link to="/login">let me in →</Link>
-        </>
-      )}
     </section>
   );
 }
